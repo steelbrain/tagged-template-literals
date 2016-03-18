e07d6c72f32de79d9ad671ae585bb7f259bd0701
steelbrain <de.steelbrain@gmail.com>
2016-03-18 04:05:08 -0700

    :white_check_mark: Add specifications

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,1 +1,26 @@
 'use babel'
+
+import tag from '../'
+import escape from 'escape-html'
+
+describe('tagged-template-literals', function() {
+  function proxy(strings, ...values) {
+    return tag(strings, values)
+  }
+  function escapingProxy(strings, ...values) {
+    return tag(strings, values, escape)
+  }
+
+  it('merges strings and values properly', function() {
+    expect(proxy`Hello ${'Dolly'}`).toBe('Hello Dolly')
+  })
+  it('works well even on all value stuff', function() {
+    expect(proxy`${'Hello'}`).toBe('Hello')
+  })
+  it('works well even on all string stuff', function() {
+    expect(proxy`Hello Dolly`).toBe('Hello Dolly')
+  })
+  it('works well with filtering callbacks', function() {
+    expect(escapingProxy`Hello ${'<div />'}`).toBe('Hello &lt;div /&gt;')
+  })
+})
